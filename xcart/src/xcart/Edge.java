984eb3a15d354f96dc0277f3fe8fbb23a4db0239
hetal177 <hetal177@gmail.com>
2021-11-10 21:13:58 +0000

    x-cart was deleted by misctake so redone edge class

--- xcart/src/xcart/Edge.java
+++ xcart/src/xcart/Edge.java
@@ -1,23 +1,23 @@
 package xcart;
 
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
-
-    public class Edge {
-        public static void main(String[] args) {
-            String baseUrl="https://www.x-cart.com";
-            System.setProperty("webdriver.edge.driver","drivers/msedgedriver.exe");
-            WebDriver driver=new EdgeDriver();
-            driver.get(baseUrl);
-            driver.manage().window().maximize();
-            String getTitle=driver.getTitle();
-            System.out.println(getTitle);
-            driver.close();
-
-        }
-
-
+public class Edge{
+    public static void main(String[]args){
+        System.setProperty("webdriver.edge.driver","drivers/msedgedriver.exe");
+        WebDriver driver=new EdgeDriver();
+        driver.get("https://www.x-cart.com");
+        driver.manage().window().maximize();
+        driver.close();
     }
+
+
+}
+
+        
+
+
 
 
